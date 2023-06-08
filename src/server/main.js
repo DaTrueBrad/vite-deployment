a4ea3be8cea01e777684922feea365b3a9278fba
Brady Bott <bradybott@Bradys-MBP.localdomain>
2023-06-08 10:37:08 -0600

    db testing

--- src/server/main.js
+++ src/server/main.js
@@ -4,6 +4,7 @@ const {Info} = require('./models')
 const db = require('./database')
 
 const app = express();
+app.use(express.json())
 
 app.get("/api/hello", (req, res) => {
   res.send("Your Mom (lol)");
